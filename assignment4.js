42a2b4ea98ce42655cc56aa6f5c93daf60a01de6
Brandon Torchia <bmt35@pitt.edu>
2017-04-17 21:46:40 -0500

    done

--- assignment4.js
+++ assignment4.js
@@ -11,7 +11,39 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
-(function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
-})();
+
+$(document).ready(function() {
+	$('.flexsearch-input').on("input", function() {
+		if($(".flexsearch-input").val().length!=0) //if there is some text in the search box
+		{
+			var info = $('.flexsearch-input').val();
+			info=info.toLowerCase();
+			$(".choices").html("");	
+			getInfo(info);	
+		} 
+	});
+	
+});
+
+function getInfo(info) {
+	$.ajax({url:"http://www.mattbowytz.com/simple_api.json?data=all",type:"GET",dataType:"json"}).done(function(json){
+		json.data.programming.forEach(function(returned_results){
+			returned_results=returned_results.toLowerCase();
+			if(returned_results.startsWith(info)){
+				$(".choices").append("<p>"+returned_results+"</p");
+			}
+		});
+	})
+
+	$.ajax({url:"http://www.mattbowytz.com/simple_api.json?data=all",type:"GET",dataType:"json"}).done(function(json){
+		json.data.interests.forEach(function(returned_results){
+			returned_results=returned_results.toLowerCase();
+			if(returned_results.startsWith(info)){
+				$(".choices").append("<p>"+returned_results+"</p");
+			}
+		});
+	})
+
+
+	// body...
+};
